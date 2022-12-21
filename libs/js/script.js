886eda10aa677d81d8cf0be3af230a33eeba0ab1
punSavitri <punshakshi@gmail.com>
2022-12-21 12:56:37 +0000

    some files added

---
+++ libs/js/script.js
@@ -0,0 +1,75 @@
+ 
+$(document).ready(function(){
+      $('#submitbtn').click(function() {
+        $.ajax({
+          url:'libs/php/findNearByWeather.php',
+          type:'POST',
+          dataType:'json',
+          data: {
+            lat: $('#lat').val(),
+            lng: $('#lng').val(),
+        },
+        success: function(result) {
+            console.log(result);
+          $('#stationname').html(result['data']['stationName']);
+          $('#temperature').html(result['data']['temperature']);
+          $('#humidity').html(result['data']['humidity']);
+        },
+        error: function (err) {
+            console.log(err);
+        },
+    });
+  }); 
+    
+  $('#earthquakesBtn').click(function() {
+    
+    $.ajax({
+      url:'libs/php/earthquakesInfo.php',
+      type:'POST',
+      dataType:'json',
+      data: {
+        north: $('#north').val(),
+        south: $('#south').val(),
+        east: $('#east').val(),
+        west: $('#west').val(),
+      },
+      success: function(result) {
+          console.log(result);
+        $('#datetime').html(result['data'][0]['datetime']);
+        $('#depth').html(result['data'][0]['depth']);
+        $('#magnitude').html(result['data'][0]['magnitude']);
+        $('#latitude').html(result['data'][0]['lat']);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
+  });
+
+  $('#weatherBtn').click(function() {
+    $.ajax({
+      url:'libs/php/weatherInfo.php',
+      type:'POST',
+      dataType:'json',
+      data: {
+        north: $('#north').val(),
+        south: $('#south').val(),
+        east: $('#east').val(),
+        west: $('#west').val(),
+      },
+      success: function(result) {
+          console.log(result);
+        $('#stationname').html(result['data'][0]['stationName']);
+        $('#temperature').html(result['data'][0]['temperature']);
+        $('#humidity').html(result['data'][0]['humidity']);
+        $('#clouds').html(result['data'][0]['clouds']);
+        $('#weathercondition').html(result['data'][0]['weatherCondition']);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
+  });
+});
+
+
