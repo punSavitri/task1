b56ca69d7399dadcfb60c40c69f1ccf2457ba647
SavitriPun <punshakshi@gmail.com>
2026-06-02 18:02:17 +0100

    update code for clean and maintable in future

---
+++ libs/js/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+  $("#submitbtn").click(function () {
+    $.ajax({
+      url: "libs/php/findNearByWeather.php",
+      type: "POST",
+      dataType: "json",
+      data: {
+        lat: $("#lat").val(),
+        lng: $("#lng").val(),
+      },
+      success: function (result) {
+        console.log(result);
+
+        $("#stationname").html(result["data"]["stationName"]);
+        $("#temperature").html(result["data"]["temperature"]);
+        $("#humidity").html(result["data"]["humidity"]);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
+  });
+
+  $("#earthquakesBtn").click(function () {
+    $.ajax({
+      url: "libs/php/earthquakesInfo.php",
+      type: "POST",
+      dataType: "json",
+      data: {
+        north: $("#earthnorth").val(),
+        south: $("#earthsouth").val(),
+        east: $("#eartheast").val(),
+        west: $("#earthwest").val(),
+      },
+      success: function (result) {
+        console.log(result);
+        $("#datetime").html(result["data"][0]["datetime"]);
+        $("#depth").html(result["data"][0]["depth"]);
+        $("#magnitude").html(result["data"][0]["magnitude"]);
+        $("#latitude").html(result["data"][0]["lat"]);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
+  });
+
+  $("#weatherBtn").click(function () {
+    $.ajax({
+      url: "libs/php/weatherInfo.php",
+      type: "POST",
+      dataType: "json",
+      data: {
+        north: $("#north").val(),
+        south: $("#south").val(),
+        east: $("#east").val(),
+        west: $("#west").val(),
+      },
+      success: function (result) {
+        console.log(result);
+        $("#speed").html(result["data"][0]["windSpeed"]);
+        $("#observe").html(result["data"][0]["observation"]);
+        $("#direction").html(result["data"][0]["windDirection"]);
+        $("#cloud").html(result["data"][0]["clouds"]);
+        $("#condition").html(result["data"][0]["weatherCondition"]);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
+  });
+});
